011096fb8d156b35859cdc7823f9744d76238f46
vera_chernevich <9111402516v@gmail.com>
2025-02-19 22:12:38 +0300

    Добавлена страница Блоги

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 import Header from "./Header.js";
 import TabsCollection from "./Tabs.js";
 import VodeoPlayerCollection from "./VideoPlayer.js";
+import ExpandableContentCollection from "./ExpandableContent.js";
 
 new Header();
 new TabsCollection();
 new VodeoPlayerCollection();
+new ExpandableContentCollection();
